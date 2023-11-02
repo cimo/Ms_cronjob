2d67c5ec138d680c5bed7fc54a29f6c7767e74d8
simone.dagostino <simone.dagostino@jp.kpmg.com>
2023-11-02 10:45:54 +0900

    update

--- application/tls/script/starter.sh
+++ application/tls/script/starter.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+rm /home/root/application/tls/certificate/tls.key
+rm /home/root/application/tls/certificate/tls.crt
+
 openssl req -x509 -newkey rsa:4096 -sha256 -days 365 -nodes \
     -keyout /home/root/application/tls/certificate/tls.key \
-    -out /home/root/application/tls/certificate/tls.crt -subj "/C=JP/ST=Tokyo/L=Tokyo/O=CIMO/OU=CIMO/CN=$DOMAIN" > /var/log/ms_cronjob/tls/starter.log 2>&1
+    -out /home/root/application/tls/certificate/tls.crt \
+    -addext "subjectAltName=DNS:localhost" \
+    -subj "/C=JP/ST=Tokyo/L=Tokyo/O=CIMO/OU=CIMO/CN=$DOMAIN" > /var/log/ms_cronjob/tls/starter.log 2>&1
 
 chmod 0644 /home/root/application/tls/certificate/tls.key
