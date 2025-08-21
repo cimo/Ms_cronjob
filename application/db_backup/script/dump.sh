61efa6463b904b6ba1ac9925df40c40526a19e52
simone.dagostino <simone.dagostino@jp.kpmg.com>
2025-08-21 15:52:54 +0900

    update

--- application/db_backup/script/dump.sh
+++ application/db_backup/script/dump.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 path="${PATH_ROOT}file_share/db_backup/"
-filename="${DB_NAME}-$(date +%Y-%m-%d).dump"
+fileName="${DB_NAME}-$(date +%Y-%m-%d).dump"
 
 echo "Dump ${DB_NAME}:\n"
 
-PGPASSWORD="${DB_PASS}" pg_dump -Fc -v -h "${DB_HOST}" -U "${DB_USER}" -d "${DB_NAME}" -f "${path}${filename}"
+PGPASSWORD="${DB_PASS}" pg_dump -Fc -v -h "${DB_HOST}" -U "${DB_USER}" -d "${DB_NAME}" -f "${path}${fileName}"
 
 find "${path}" -name "${DB_NAME}-*.dump" -type f -mtime +7 -delete
