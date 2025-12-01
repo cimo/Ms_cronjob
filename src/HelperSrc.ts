7b3417cc006472820ee11855e093c27882bf4dee
simone.dagostino <simone.dagostino@jp.kpmg.com>
2025-12-01 15:05:25 +0900

    update

--- src/HelperSrc.ts
+++ src/HelperSrc.ts
@@ -33,21 +33,21 @@ const localeConfiguration: Record<string, { locale: string; currency: string }> 
     nz: { locale: "mi-NZ", currency: "NZD" }
 };
 
-export const ENV_NAME = Ce.checkVariable("ENV_NAME") || (process.env.ENV_NAME as string);
+export const ENV_NAME = Ce.checkVariable("ENV_NAME") || (process.env["ENV_NAME"] as string);
 
 Ce.loadFile(`./env/${ENV_NAME}.env`);
 
-export const DOMAIN = Ce.checkVariable("DOMAIN") || (process.env.DOMAIN as string);
-export const TIME_ZONE = Ce.checkVariable("TIME_ZONE") || (process.env.TIME_ZONE as string);
-export const LANG = Ce.checkVariable("LANG") || (process.env.LANG as string);
-export const SERVER_PORT = Ce.checkVariable("SERVER_PORT") || (process.env.SERVER_PORT as string);
+export const DOMAIN = Ce.checkVariable("DOMAIN") || (process.env["DOMAIN"] as string);
+export const TIME_ZONE = Ce.checkVariable("TIME_ZONE") || (process.env["TIME_ZONE"] as string);
+export const LANG = Ce.checkVariable("LANG") || (process.env["LANG"] as string);
+export const SERVER_PORT = Ce.checkVariable("SERVER_PORT") || (process.env["SERVER_PORT"] as string);
 export const PATH_ROOT = Ce.checkVariable("PATH_ROOT");
-export const NAME = Ce.checkVariable("MS_C_NAME") || (process.env.MS_O_NAME as string);
-export const LABEL = Ce.checkVariable("MS_C_LABEL") || (process.env.MS_O_LABEL as string);
-export const IS_DEBUG = Ce.checkVariable("MS_C_IS_DEBUG") || (process.env.MS_O_IS_DEBUG as string);
-export const NODE_ENV = Ce.checkVariable("MS_C_NODE_ENV") || (process.env.MS_O_NODE_ENV as string);
-export const URL_ROOT = Ce.checkVariable("MS_C_URL_ROOT") || (process.env.MS_O_URL_ROOT as string);
-export const URL_CORS_ORIGIN = Ce.checkVariable("MS_C_URL_CORS_ORIGIN") || (process.env.MS_O_URL_CORS_ORIGIN as string);
+export const NAME = Ce.checkVariable("MS_C_NAME") || (process.env["MS_O_NAME"] as string);
+export const LABEL = Ce.checkVariable("MS_C_LABEL") || (process.env["MS_O_LABEL"] as string);
+export const IS_DEBUG = Ce.checkVariable("MS_C_IS_DEBUG") || (process.env["MS_O_IS_DEBUG"] as string);
+export const NODE_ENV = Ce.checkVariable("MS_C_NODE_ENV") || (process.env["MS_O_NODE_ENV"] as string);
+export const URL_ROOT = Ce.checkVariable("MS_C_URL_ROOT") || (process.env["MS_O_URL_ROOT"] as string);
+export const URL_CORS_ORIGIN = Ce.checkVariable("MS_C_URL_CORS_ORIGIN") || (process.env["MS_O_URL_CORS_ORIGIN"] as string);
 export const PATH_CERTIFICATE_KEY = Ce.checkVariable("MS_C_PATH_CERTIFICATE_KEY");
 export const PATH_CERTIFICATE_CRT = Ce.checkVariable("MS_C_PATH_CERTIFICATE_CRT");
 export const PATH_FILE = Ce.checkVariable("MS_C_PATH_FILE");
