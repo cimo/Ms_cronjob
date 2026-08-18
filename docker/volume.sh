0ca2cc08f1c11e6529c23eafbb1c57f7d930fd8a
simone.dagostino <simone.dagostino@jp.kpmg.com>
2026-08-18 18:06:03 +0900

    update

--- docker/volume.sh
+++ docker/volume.sh
@@ -17,7 +17,7 @@ docker run --rm \
 -v "cimo_${parameter1}_ms_cronjob-volume:/home/target/" \
 -v "$(pwd)/certificate/:/home/source/:ro" \
 alpine sh -c '
-cp "/home/source/ca.key" "/home/source/ca.pem" "/home/source/ca.srl" "/home/source/tls.crt" "/home/source/tls.key" "/home/target/" &&
-chown "$HOST_UID:$HOST_GID" "/home/target/ca.key" "/home/target/ca.pem" "/home/target/ca.srl" "/home/target/tls.crt" "/home/target/tls.key" &&
-chmod 644 "/home/target/ca.pem" "/home/target/ca.srl" "/home/target/tls.crt" && chmod 600 "/home/target/ca.key" "/home/target/tls.key"
+cp "/home/source/ca.key" "/home/source/ca.pem" "/home/source/tls.crt" "/home/source/tls.key" "/home/target/" &&
+chown "$HOST_UID:$HOST_GID" "/home/target/ca.key" "/home/target/ca.pem" "/home/target/tls.crt" "/home/target/tls.key" &&
+chmod 644 "/home/target/ca.pem" "/home/target/tls.crt" && chmod 600 "/home/target/ca.key" "/home/target/tls.key"
 '
